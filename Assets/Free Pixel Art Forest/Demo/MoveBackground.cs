333a2983fc8e6b5feb15fcb9a5c09da75c9ebf5d
Cassandra P DeBlois <47582046+Sharmena@users.noreply.github.com>
2022-11-09 16:21:19 -0500

    Initial Comit
    Setting up unity with github

---
+++ Assets/Free Pixel Art Forest/Demo/MoveBackground.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBackground : MonoBehaviour {
+
+
+
+	public float speed;
+	private float x;
+	public float PontoDeDestino;
+	public float PontoOriginal;
+	public float playerInput;
+
+
+
+
+	// Use this for initialization
+	void Start () {
+		//PontoOriginal = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		playerInput = Input.GetAxis("Horizontal");
+
+		x = transform.position.x;
+		x += speed * Time.deltaTime * playerInput;
+		transform.position = new Vector3 (x, transform.position.y, transform.position.z);
+
+
+
+		if (x <= PontoDeDestino){
+
+			Debug.Log ("hhhh");
+			x = PontoOriginal;
+			transform.position = new Vector3 (x, transform.position.y, transform.position.z);
+		}
+
+
+	}
+}
