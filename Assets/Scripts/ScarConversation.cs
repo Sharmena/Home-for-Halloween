38f039e34ee443b719f95d010fd2c9a2194e2097
Cassandra P DeBlois <47582046+Sharmena@users.noreply.github.com>
2022-12-01 15:07:56 -0500

    New Cards
    Added New cards for player and scar

---
+++ Assets/Scripts/ScarConversation.cs
@@ -0,0 +1,39 @@
+using Fluent;
+using TMPro;
+using UnityEngine.UI;
+
+/// <summary>
+/// This example introduces writing text to the dialog.
+/// You also need a WriteHandler to specify the Text UI element to write to.
+/// Write() will write some text with a pause at the end.
+/// </summary>
+public class ScarConversation : MyFluentDialogue
+{
+    public TextMeshProUGUI OtherTextElement;
+
+    public override void OnFinish()
+    {
+        OtherTextElement.text = "";
+        base.OnFinish();
+    }
+
+    public override FluentNode Create()
+    {
+        return
+            Show() *
+            Write(0, "Hello? I’m glad you’re awake. We wandered a bit too far away from home and I’m not sure we can get there in time for Halloween dinner. Do you think you could get us back?") *
+
+            Options
+            (
+                Option("Sure thing!") *
+                    Write(0.5f, "Really? I just don’t know if we will make it…") *
+                    
+                    Options
+                    (
+                        Option("Let me cheer you up (Start card game)") *
+
+                    End()
+                    )
+             );
+    }
+}
