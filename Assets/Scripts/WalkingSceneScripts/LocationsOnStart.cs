d703392d02c70b72191046fda5a315d78825a830
Cassandra P DeBlois <47582046+Sharmena@users.noreply.github.com>
2022-12-20 16:28:07 -0500

    Fixed Wizard Animation

--- Assets/Scripts/WalkingSceneScripts/LocationsOnStart.cs
+++ Assets/Scripts/WalkingSceneScripts/LocationsOnStart.cs
@@ -21,12 +21,29 @@ public class LocationsOnStart : MonoBehaviour
         if (DefeatManager.scarDefeat == true && DefeatManager.scarFollowing)
         {
             scar.transform.position = new Vector3(-2.17f, -0.65f, 0f);
+            scar.GetComponentInChildren<SpriteRenderer>().flipX = true;
+
         } else if (DefeatManager.scarDefeat == true && DefeatManager.scarFollowing != true)
         {
             scar.transform.position = new Vector3(2.357f, -0.65f, 0f);
             scar.GetComponent<Animator>().Play("ScarFollow");
-            DefeatManager.scarFollowing = true;
+            DefeatManager.scarFollowing = true;            
         }
+
+        if (DefeatManager.wizardDefeat == true && DefeatManager.wizardFollowing)
+        {
+            wizard.transform.position = new Vector3(-2.17f, -0.65f, 0f);
+            wizard.GetComponentInChildren<SpriteRenderer>().flipX = true;
+        }
+        else if (DefeatManager.wizardDefeat == true && DefeatManager.wizardFollowing != true)
+        {
+            wizard.transform.position = new Vector3(2.357f, -0.65f, 0f);
+            wizard.GetComponent<Animator>().Play("WizardFollow");
+            DefeatManager.wizardFollowing = true;
+            
+        }
+
+
     }
 
     // Update is called once per frame
